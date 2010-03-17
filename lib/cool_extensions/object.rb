7ef69bba42ac84796175d08c96dadd07aef385aa
Jason Amster <jayamster@gmail.com>
2010-03-17 12:17:51 -0400

    renaming jtest to test_fputs

--- lib/cool_extensions/object.rb
+++ lib/cool_extensions/object.rb
@@ -3,7 +3,6 @@ class Object
     # move cursor to beginning of line
     cr = "\r"           
 
-
     # ANSI escape code to clear line from cursor to end of line
     # "\e" is an alternative to "\033"
     # cf. http://en.wikipedia.org/wiki/ANSI_escape_code
